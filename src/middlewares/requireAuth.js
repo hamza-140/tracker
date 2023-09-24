be7a4da6bdcecb018f57045c06b78bf171f0aca1
hamza-140 <sahamzashah19@gmail.com>
2023-09-25 02:32:57 +0500

    make it better

--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -5,7 +5,7 @@ const User = mongoose.model("User");
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(401).send({ error: "You must be logged in." });
+    return res.status(401).send({ error: "You must be logged in" });
   }
 
   const token = authorization.replace("Bearer ", "");
